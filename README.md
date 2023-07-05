1b040bc9382afc2a389070d73994426113120eba
dhruv035 <dhruv035@gmail.com>
2023-07-05 05:53:14 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+A simple typescript express server that uses graphclient to query the subgraph and return results
+
+Steps to run
+
+1. Build the project with the following command
+npm run build
+
+2. Start the server locally with the following command
+npm start
---
+++ index.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from 'express';
+import { ExampleQueryDocument, ExampleQueryQuery, execute } from './.graphclient/index'
+const app: Express = express();
+app.get('/', (req: Request, res: Response) => {
+    execute(ExampleQueryDocument, {}).then((result)=>{
+        res.json(result)
+    })
+  });
+
+app.listen(3000, () => {
+    console.log("Listen on the port 3000...");
+});
